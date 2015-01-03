deafa28c2c25f67dafbe46fe5d91fc69fd406bef
shamHu <shamHu@gmail.com>
2015-01-02 21:15:46 -0800

    Basic Card and Deck functionality
    Day 1 stuff Cards and Decks

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour{
+
+	[SerializeField]
+	private int cardValue;
+	public int CardValue
+	{
+		get { return cardValue; }
+		set { cardValue = value; }
+	}
+
+	[SerializeField]
+	private int cardSuit;
+	public int CardSuit
+	{
+		get { return cardSuit; }
+		set { cardSuit = value; }
+	}
+
+	[SerializeField]
+	private bool faceUp;
+	public bool FaceUp
+	{
+		get { return faceUp; }
+		set { faceUp = value; }
+	}
+
+	public Card(int value, int suit, bool faceUp)
+	{
+		this.cardValue = value;
+		this.cardSuit = suit;
+		this.faceUp = faceUp;
+	}
+
+
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour {
+
+	private List<GameObject> deckList = new List<GameObject>();
+	public List<GameObject> DeckList
+	{
+		get { return deckList; }
+		set { deckList = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i <= 13; i++) {
+			for (int j = 0; j < 4; j++) {
+				GameObject newCard = (GameObject) Instantiate (Resources.Load("CardPlane"));
+				newCard.GetComponent<Card>().CardValue = i;
+				newCard.GetComponent<Card>().CardSuit = j;
+				newCard.GetComponent<Card>().FaceUp = false;
+				newCard.transform.parent = this.transform;
+
+				deckList.Add (newCard);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
